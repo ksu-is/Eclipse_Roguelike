2399f49433d9d5f66dc3c9bb3dc8471fb732fbb1
Dpagel <dpagel1@students.kennesaw.edu>
2022-04-26 03:43:25 -0400

    Update game_map.py

--- Project/game_map.py
+++ Project/game_map.py
@@ -7,7 +7,7 @@ class GameMap:
     def __init__(self, width: int, height: int):
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
-        
+
     def in_bounds(self, x:int, y:int) -> bool:
         return 0 <= x < self.width and 0 <= y < self.height
 
